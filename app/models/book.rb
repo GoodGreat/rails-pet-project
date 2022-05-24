184d04eeb2c942e4a891ba74d010aa1dd6a34d59
Alvaro <alvaro96rojo@gmail.com>
2022-05-24 15:49:51 +0200

    Define and test model validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
     belongs_to :author
+    validates_associated :author
+    validates :title, presence: true
+    validates :genre, presence: true
+    validates :num_pages, :numericality => { greater_than: 0 }, presence: true
 end
